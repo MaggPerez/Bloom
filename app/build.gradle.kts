783db761e664db9727773a8c1dc6f5ecd09090f7
MaggPerez <perezmag5137@gmail.com>
2025-10-28 23:40:12 -0400

    currently installing supabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
@@ -16,6 +19,13 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        //set value part for supabase
+        Properties properties = new Properties()
+        properties.load(project.rootProject.file("local.properties").newDataInputStream())
+        buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"${properties.getProperty("SUPABASE_PUBLISHABLE_KEY")}\"")
+        buildConfigField("String", "SECRET", "\"${properties.getProperty("SECRET")}\"")
+        buildConfigField("String", "SUPABASE_URL", "\"${properties.getProperty("SUPABASE_URL")}\"")
     }
 
     buildTypes {
@@ -58,4 +68,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //supabase
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.3.1"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+
+    implementation("io.ktor:ktor-client-okhttp:3.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
---
+++ app/src/main/java/com/example/bloom/SupabaseClient.kt
@@ -0,0 +1,19 @@
+package com.example.bloom
+
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.realtime.Realtime
+
+
+object SupabaseClient {
+    val client: SupabaseClient = createSupabaseClient(
+        supabaseUrl = "YOUR_SUPABASE_URL",
+        supabaseKey = "YOUR_SUPABASE_ANON_KEY"
+    ) {
+        install(Auth)
+        install(Postgrest)
+        install(Realtime)
+    }
+}
